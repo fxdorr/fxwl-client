100ca42f6cb07810bfed1274b8f9e2886ea8c267
唐启云 <tqy@fxri.net>
2023-08-06 07:48:30 +0800

    [新增]unplugin-auto-import包

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   main: {
@@ -15,6 +16,12 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()]
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ['vue'],
+        dts: 'auto-imports.d.ts'
+      })
+    ]
   }
 })
