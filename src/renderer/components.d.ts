995cd8dbeda59677164f4b51f08aa409ff71806e
唐启云 <tqy@fxri.net>
2023-08-06 10:00:44 +0800

    [更新]疏理首页内容 [更新]格式化代码

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -6,8 +6,19 @@
 export {}
 
 declare module 'vue' {
-    export interface GlobalComponents {
-        ElButton: typeof import('element-plus/es')['ElButton']
-        Versions: typeof import('./src/components/Versions.vue')['default']
-    }
+  export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDropdown: typeof import('element-plus/es')['ElDropdown']
+    ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
+    ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElFooter: typeof import('element-plus/es')['ElFooter']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    Head: typeof import('./src/components/base/Head.vue')['default']
+    Header: typeof import('./src/components/base/Header.vue')['default']
+    Versions: typeof import('./src/components/Versions.vue')['default']
+  }
 }
---
+++ src/renderer/src/components/base/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="fxyui-head">
+        方弦物联-客户端
+    </div>
+</template>
+<style lang="scss">
+    .fxyui-head {
+        text-align: center;
+    }
+</style>
